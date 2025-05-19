ab28e2d9a136fd6f5fb7f21377d4827ade773fb8
pawan kumar <113826393+Pawangupta123@users.noreply.github.com>
2025-05-19 09:04:58 +0000

    b

---
+++ 01_basic/datatype.js
@@ -0,0 +1,18 @@
+let name = "Pawan";
+let number = 2022;
+let ispn = false;
+let state;
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//obejects
+
+console.log(typeof undefined);
+console.log(typeof null);
+// undefine is = undefine
+//null is = obeject
