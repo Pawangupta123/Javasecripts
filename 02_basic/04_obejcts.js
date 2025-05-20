74a6aa0475f5ef485dd6ce35eb9c47565972c87d
pawan kumar <113826393+Pawangupta123@users.noreply.github.com>
2025-05-20 18:46:42 +0000

    intial

--- 02_basic/04_obejcts.js
+++ 02_basic/04_obejcts.js
@@ -22,14 +22,26 @@ const obj2 = {3:"a",4:"b"};
 const obj4 = {5:"a", 6:"b"};
 //let obej3 = Object.assign({},obj1,obj2,obj4);//merger 3no obejects
 const obej3 = {...obj1,...obj2,...obj4};
-console.log(obej3);
+// console.log(obej3);
 
-console.log(tinderUser);
-console.log(tinderUser.name);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));//[ [ 'id', '123abc' ], [ 'name', 'lolo' ], [ 'islogin', false ] ]
-console.log(tinderUser.hasOwnProperty('islogin'));
+// console.log(tinderUser);
+// console.log(tinderUser.name);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));//[ [ 'id', '123abc' ], [ 'name', 'lolo' ], [ 'islogin', false ] ]
+// console.log(tinderUser.hasOwnProperty('islogin'));
 
 
 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
