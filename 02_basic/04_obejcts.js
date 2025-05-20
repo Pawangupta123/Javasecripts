f929f69594b4bfa6e728bee56250073f30c2f010
pawan kumar <113826393+Pawangupta123@users.noreply.github.com>
2025-05-20 18:30:00 +0000

    intial commit

---
+++ 02_basic/04_obejcts.js
@@ -0,0 +1,35 @@
+// const tinderUser = new Object()
+const tinderUser={}
+
+tinderUser.id = "123abc";
+tinderUser.name ="lolo";
+tinderUser.islogin = false;
+
+// console.log(tinderUser);
+const regularUser={
+    email: "pg@gameil.com",
+    fullName:{
+        UserFullname:{
+            firstName:"Pawan",
+            last:"kumar"
+        }
+    }
+}
+console.log(regularUser.fullName.UserFullname.firstName);
+
+const obj1 = {1:"a",2:"b"};
+const obj2 = {3:"a",4:"b"};
+const obj4 = {5:"a", 6:"b"};
+//let obej3 = Object.assign({},obj1,obj2,obj4);//merger 3no obejects
+const obej3 = {...obj1,...obj2,...obj4};
+console.log(obej3);
+
+console.log(tinderUser);
+console.log(tinderUser.name);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));//[ [ 'id', '123abc' ], [ 'name', 'lolo' ], [ 'islogin', false ] ]
+console.log(tinderUser.hasOwnProperty('islogin'));
+
+
+
