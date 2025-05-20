702cd5d337202991d79a6138c3e1e2303cdf67a8
pawan kumar <113826393+Pawangupta123@users.noreply.github.com>
2025-05-20 06:46:51 +0000

    final

---
+++ 01_basic/06_string.js
@@ -0,0 +1,35 @@
+const name = "Pawan";
+const repoCOunt = 40;
+//console.log(name + repoCOunt + " value");
+// console.log(`Hello my name is ${name} and mai ho gean ${repoCOunt}`);
+
+const gameName = new String('xoxo-xc-rcb');
+// console.log(gameName[0]);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('o'));
+const newString = gameName.substring(0,3); 
+console.log(newString);
+const anotherString = gameName.slice(-2,0);// print reverse order
+console.log(anotherString);
+const  newStringOne = ' Panwa ';
+console.log(newStringOne);
+console.log(newStringOne.trim());
+//console.log(newStringOne.trimEnd());
+
+const url = 'pawankumar'
+console.log(url.replace('w', '-'));// replace the 'w' in '-'
+console.log(url.includes('gupta')); //find the word with help of gupta
+
+console.log(gameName.split('-'));//convert string too array
+
+
+
+
+
+
+
+
+
--- 01_basic/finalSummeri.js
+++ 01_basic/finalSummeri.js
@@ -1,13 +1,13 @@
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol(unique), BigInt
-const sc =100;
+const sc = 100;
 const scValue = 100.3
-const temp= null;
+const temp = null;
 let userEmail;
 const id = Symbol("123");
 const anotherId = Symbol("123");
-console.log((id===anotherId));
+console.log((id === anotherId));
 const BigNumber = 462776n;
 
 // Reference (Non primitive)
@@ -15,15 +15,31 @@ const BigNumber = 462776n;
 // Array, Objects, Functions
 
 const heros = ["pawan", "fuli", "deapak"];
-let Myobej ={
+let Myobej = {
 
-    name:"pawa",
-    age: 35,
+  name: "pawa",
+  age: 35,
 }
-const myfunction = function(){
+const myfunction = function () {
   console.log("Ram Ram");
 }
-console.log(typeof(temp));
+console.log(typeof (temp));
 
+/*"""""""""""""""""""""""""""""""""""""""""""stackand Heap Memory''""""""""""""""""""""
+*/
 
+//Stack(Primitive Type),Heap(Non Primitive  data types)
+let myutube = "chaiwithcode";
+let anotherName = "pwn";
+anotherName = "guppwn@gmail.com";
+console.log(myutube);
+console.log(anotherName);
+let userOne = {
+  Email: "pwn@gmail.com",
+  passward: 7366,
+}
+let userTwo= userOne;
+userTwo.Email = "gu.gail.com";
+console.log(userOne.Email);
+console.log(userTwo.Email);
 
