0d818c54c9c192967f78b45167a24980cf708155
pawan kumar <113826393+Pawangupta123@users.noreply.github.com>
2025-05-23 08:17:07 +0000

    while and do- while-loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,15 @@
+// 
+
+const myarr=["apple","banana","orange"];
+let arr =0;
+while (arr<myarr.length){
+    console.log(myarr[arr]);
+    arr++;
+    
+}
+let sc =100;
+
+do{
+    console.log(`the value of score:${sc}`);
+    sc++;
+}while(sc<105);
